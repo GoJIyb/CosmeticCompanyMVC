59764a5ca952bd28e2bc2f6e165dbc487ac65a47
Petro <GoJIybNarik@gmail.com>
2026-05-31 15:07:54 +0300

    Initial commit

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using CosmeticCompanyMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace CosmeticCompanyMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Ласкаво просимо до оформлення замовлення!";
+
+            Product product = new Product
+            {
+                Name = "Крем для обличчя",
+                Price = 450,
+                Description = "Професійний зволожуючий крем"
+            };
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            return View("Result", product);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using CosmeticCompanyMVC.Data;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static System.Net.Mime.MediaTypeNames;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
